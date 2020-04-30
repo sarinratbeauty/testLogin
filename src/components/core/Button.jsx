2ad7a04c59add52ae92872c17ec61a3399320cc4
sarinratbeauty <sarinrat.beauty@mail.kmutt.ac.th>
2020-05-01 03:05:30 +0700

    🦥[ADD] button jsx

---
+++ src/components/core/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function coreButton(props) {
+  return (
+    <button type="button" className="button col-12 font-weight-bold">
+      {props.text}
+    </button>
+  );
+}
