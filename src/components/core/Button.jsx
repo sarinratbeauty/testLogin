a2b094012376c08389c9389c28570322a397f460
sarinratbeauty <sarinrat.beauty@mail.kmutt.ac.th>
2020-05-05 02:25:05 +0700

    🦥[UPDATE] button

--- src/components/core/Button.jsx
+++ src/components/core/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function coreButton(props) {
   return (
-    <button type="button" className="button col-12 font-weight-bold">
+    <button type="button" className="button col-12 font-weight-bold" onClick={props.onClick}>
       {props.text}
     </button>
   );
